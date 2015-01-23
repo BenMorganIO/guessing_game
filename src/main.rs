7e5f2d15f386b5ed70638dc3757bab0783e58172
Ben A Morgan <ben@benmorgan.io>
2015-01-22 22:49:45 -0800

    remove printing secret number

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
   let secret_number: uint = (rand::random::<uint>() % 100u) + 1u;
 
-  println!("The secret number is: {}", secret_number);
-
   loop {
     println!("Please input your guess.");
 
