698bd5b74b86daf8edda07cea103d2c9bff8b708
Ben A Morgan <ben@benmorgan.io>
2015-01-22 22:29:51 -0800

    generating secret number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::io;
+use std::rand;
 
 fn main() {
   println!("Guess the number!");
 
+  let secret_number: uint = (rand::random::<uint>() % 100u) + 1u;
+
+  println!("The secret number is: {}", secret_number);
+
   println!("Please input your guess.");
 
-  let input = io::stdn().read_line().ok().expect("Failed to read line");
+  let input = io::stdin().read_line().ok().expect("Failed to read line");
 
   println!("You guessed: {}", input);
 }
