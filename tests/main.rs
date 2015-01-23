8d43565e9858b626b15b28cea019e34f0ea9b9ad
Ben A Morgan <ben@benmorgan.io>
2015-01-22 23:09:10 -0800

    add notes on testing

---
+++ tests/main.rs
@@ -0,0 +1,10 @@
+extern crate guessing_game;
+
+mod tests {
+  // I'm assuming the io will need some mocking/stubbing,
+  // so these tests need to wait.
+  // There doesn't seem to be any info online about testing
+  // simple `println!`s.
+  // Was hoping to at least test the `cmp` function, but
+  // I'd need to mock the Ordering module...
+}
