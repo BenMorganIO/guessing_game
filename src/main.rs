0a56adc09635b3649800c3c07867a48e7fefff6a
Ben A Morgan <ben@benmorgan.io>
2015-01-22 22:22:20 -0800

    processing a guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-  println!("Hello, world!");
+  println!("Guess the number!");
+
+  println!("Please input your guess.");
+
+  let input = io::stdn().read_line().ok().expect("Failed to read line");
+
+  println!("You guessed: {}", input);
 }
